90c3294a44f3a34eba67d591682f550ab464d8e3
PablosGonUMA <pablosgon@uma.es>
2023-02-11 14:17:55 +0100

    Definición de los casos de prueba del constructor Person

--- src/test/java/org/example/PersonTest.java
+++ src/test/java/org/example/PersonTest.java
@@ -1,6 +1,16 @@
 package org.example;
 
+/**
+ * CASOS DE PRUEBA
+ * (1) - Si se crea un objeto Person, su nombre debe coincidir con el que se pasó por parámetro;
+ * (2) - Si se crea un objeto Person, su edad debe coincidir con la que se pasó por parámetro;
+ * (3) - Si se crea un objeto Person, su género debe coincidir con el que se pasó por parámetro;
+ * (4) - Si el género pasado por parámetro no es ni "masculino" ni "femenino" saltará una excepción;
+ */
 class PersonTest {
+
+    Person person;
+
 
 
 }
